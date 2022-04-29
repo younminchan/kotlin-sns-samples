6bbfb9031d088eb2a1d2902404d3b48e820ba5cd
younminchan <dbsalscks0@naver.com>
2022-04-29 13:02:51 +0900

    Update Naver Login API

--- kotlin-naver-login/app/src/main/java/com/example/kotlin_naver_login/MainActivity.kt
+++ kotlin-naver-login/app/src/main/java/com/example/kotlin_naver_login/MainActivity.kt
@@ -2,20 +2,18 @@ package com.example.kotlin_naver_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.androidnetworking.AndroidNetworking
-import com.androidnetworking.common.Priority
-import com.androidnetworking.error.ANError
-import com.androidnetworking.interfaces.JSONObjectRequestListener
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import com.example.kotlin_naver_login.databinding.ActivityMainBinding
-import com.nhn.android.naverlogin.OAuthLogin
-import com.nhn.android.naverlogin.OAuthLoginHandler
-import org.json.JSONObject
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.NidOAuthLogin
+import com.navercorp.nid.oauth.OAuthLoginCallback
+import com.navercorp.nid.profile.NidProfileCallback
+import com.navercorp.nid.profile.data.NidProfileResponse
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-    //네이버 로그인
-    var mNaverLoginModule : OAuthLogin? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,53 +21,120 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         /** Naver Login Module Initialize */
-        mNaverLoginModule = OAuthLogin.getInstance()
         val naverClientId = getString(R.string.social_login_info_naver_client_id)
         val naverClientSecret = getString(R.string.social_login_info_naver_client_secret)
         val naverClientName = getString(R.string.social_login_info_naver_client_name)
-        mNaverLoginModule?.init(this, naverClientId, naverClientSecret, naverClientName)
+        NaverIdLoginSDK.initialize(this, naverClientId, naverClientSecret , naverClientName)
+
+        setLayoutState(false) 
 
         binding.tvNaverLogin.setOnClickListener {
             startNaverLogin()
         }
+        binding.tvNaverLogout.setOnClickListener {
+            startNaverLogout()
+        }
+        binding.tvNaverDeleteToken.setOnClickListener {
+            startNaverDeleteToken()
+        }
     }
+
+    /**
+     * 로그인
+     * authenticate() 메서드를 이용한 로그인 */
     private fun startNaverLogin(){
-        // 2. userId 조회(access)
-        fun getNaverUserInfo(tokenType : String?, accessToken: String?){
-            val urlStr = "https://openapi.naver.com/v1/nid/me"
-            val authorization = "$tokenType $accessToken"
+        var naverToken :String? = ""
 
-            AndroidNetworking.get(urlStr)
-                .addHeaders("Authorization", authorization)
-                .setPriority(Priority.LOW)
-                .build()
-                .getAsJSONObject(object : JSONObjectRequestListener {
-                    override fun onResponse(response: JSONObject?) {
-                        val res = response?.get("response") as JSONObject?
-                        val userId = res?.get("id") as String?
-                        binding.tvResult.text = "userId: ${userId} \n\n accessToken: ${accessToken}"
-                    }
-
-                    override fun onError(anError: ANError?) {
-
-                    }
-                })
+        val profileCallback = object : NidProfileCallback<NidProfileResponse> {
+            override fun onSuccess(response: NidProfileResponse) {
+                val userId = response.profile?.id
+                binding.tvResult.text = "id: ${userId} \ntoken: ${naverToken}"
+                setLayoutState(true)
+                Toast.makeText(this@MainActivity, "네이버 아이디 로그인 성공!", Toast.LENGTH_SHORT).show()
+            }
+            override fun onFailure(httpStatus: Int, message: String) {
+                val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+                val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+                Toast.makeText(this@MainActivity, "errorCode: ${errorCode}\n" +
+                        "errorDescription: ${errorDescription}", Toast.LENGTH_SHORT).show()
+            }
+            override fun onError(errorCode: Int, message: String) {
+                onFailure(errorCode, message)
+            }
         }
 
-//        // 1. 네이버 로그인 요청
-        mNaverLoginModule?.startOauthLoginActivity(this, object : OAuthLoginHandler() {
-            override fun run(success: Boolean) {
-                if (success) {
-                    val accessToken: String? = mNaverLoginModule?.getAccessToken(this@MainActivity)
-                    //val refreshToken: String? = mNaverOAuthLoginModule?.getRefreshToken(getActivity())
-                    val tokenType = mNaverLoginModule?.getTokenType(this@MainActivity)
+        /** OAuthLoginCallback을 authenticate() 메서드 호출 시 파라미터로 전달하거나 NidOAuthLoginButton 객체에 등록하면 인증이 종료되는 것을 확인할 수 있습니다. */
+        val oauthLoginCallback = object : OAuthLoginCallback {
+            override fun onSuccess() {
+                // 네이버 로그인 인증이 성공했을 때 수행할 코드 추가
+                naverToken = NaverIdLoginSDK.getAccessToken()
+//                var naverRefreshToken = NaverIdLoginSDK.getRefreshToken()
+//                var naverExpiresAt = NaverIdLoginSDK.getExpiresAt().toString()
+//                var naverTokenType = NaverIdLoginSDK.getTokenType()
+//                var naverState = NaverIdLoginSDK.getState().toString()
 
-                    getNaverUserInfo(tokenType, accessToken)
+                //로그인 유저 정보 가져오기
+                NidOAuthLogin().callProfileApi(profileCallback)
+            }
+            override fun onFailure(httpStatus: Int, message: String) {
+                val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+                val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+                Toast.makeText(this@MainActivity, "errorCode: ${errorCode}\n" +
+                        "errorDescription: ${errorDescription}", Toast.LENGTH_SHORT).show()
+            }
+            override fun onError(errorCode: Int, message: String) {
+                onFailure(errorCode, message)
+            }
+        }
 
-                } else {
-                    val errorDesc = mNaverLoginModule?.getLastErrorDesc(this@MainActivity)
-                }
+        NaverIdLoginSDK.authenticate(this, oauthLoginCallback)
+    }
+
+    /**
+     * 로그아웃
+     * 애플리케이션에서 로그아웃할 때는 다음과 같이 NaverIdLoginSDK.logout() 메서드를 호출합니다. */
+    private fun startNaverLogout(){
+        NaverIdLoginSDK.logout()
+        setLayoutState(false)
+        Toast.makeText(this@MainActivity, "네이버 아이디 로그아웃 성공!", Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * 연동해제
+     * 네이버 아이디와 애플리케이션의 연동을 해제하는 기능은 다음과 같이 NidOAuthLogin().callDeleteTokenApi() 메서드로 구현합니다.
+        연동을 해제하면 클라이언트에 저장된 토큰과 서버에 저장된 토큰이 모두 삭제됩니다.
+     */
+    private fun startNaverDeleteToken(){
+        NidOAuthLogin().callDeleteTokenApi(this, object : OAuthLoginCallback {
+            override fun onSuccess() {
+                //서버에서 토큰 삭제에 성공한 상태입니다.
+                setLayoutState(false)
+                Toast.makeText(this@MainActivity, "네이버 아이디 토큰삭제 성공!", Toast.LENGTH_SHORT).show()
+            }
+            override fun onFailure(httpStatus: Int, message: String) {
+                // 서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+                // 클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+                Log.d("naver", "errorCode: ${NaverIdLoginSDK.getLastErrorCode().code}")
+                Log.d("naver", "errorDesc: ${NaverIdLoginSDK.getLastErrorDescription()}")
+            }
+            override fun onError(errorCode: Int, message: String) {
+                // 서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+                // 클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+                onFailure(errorCode, message)
             }
         })
+    }
+
+    private fun setLayoutState(login: Boolean){
+        if(login){
+            binding.tvNaverLogin.visibility = View.GONE
+            binding.tvNaverLogout.visibility = View.VISIBLE
+            binding.tvNaverDeleteToken.visibility = View.VISIBLE
+        }else{
+            binding.tvNaverLogin.visibility = View.VISIBLE
+            binding.tvNaverLogout.visibility = View.GONE
+            binding.tvNaverDeleteToken.visibility = View.GONE
+            binding.tvResult.text = ""
+        }
     }
 }
